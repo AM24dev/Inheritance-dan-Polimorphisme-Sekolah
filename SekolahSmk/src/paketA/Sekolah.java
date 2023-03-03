469b2f13712cdd789e3e81b982d3c9957dbb2d63
AM24dev <118036432+AM24dev@users.noreply.github.com>
2023-03-03 08:25:56 +0700

    Add files via upload

---
+++ SekolahSmk/src/paketA/Sekolah.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paketA;
+
+/**
+ *
+ * @author ACER
+ */
+public class Sekolah {
+    protected String nama; 
+    Sekolah(){} 
+        public void info(){ 
+        System.out.println("SMK N 4 Kendal"); 
+    } 
+}
